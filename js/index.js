5a293f3f951fbaec0edb10671f47ab53f70b883a
Olena <72007221+Oleva-abr@users.noreply.github.com>
2023-10-26 19:09:51 +0300

    cleaned code

--- js/index.js
+++ js/index.js
@@ -38,9 +38,7 @@ function makeGallery(persons) {
   const marcup = persons
     ?.map(
       ({ firstname, lastname, email, phone, birthday, gender, website }) =>
-        // `<a class="gallery__item" href="${largeImageURL}"><div class="photo-card"><div class="img-container"><img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" /></div><div class="info"><p class="info-item"><b>Likes</b><br>${likes}</p><p class="info-item"><b>Views</b><br>${views}</p><p class="info-item"><b>Comments</b><br>${comments}</p><p class="info-item"><b>Downloads</b><br>${downloads}</p></div></div></a>`
         `
-            
         <div class="card">
         <div class="name">
         <div class="firstname">${firstname}</div>
